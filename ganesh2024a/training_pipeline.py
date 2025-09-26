d14125bc893d84982cd645164e267f09d5b5ebcc
whyhardt <daniel.weinhardt@uni-osnabrueck.de>
2025-09-26 14:07:39 -0400

    adjusted training for cluster

--- ganesh2024a/training_pipeline.py
+++ ganesh2024a/training_pipeline.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import torch
 
 from spice.utils.convert_dataset import convert_dataset
 from spice.utils.plotting import plot_session
@@ -26,31 +27,32 @@ estimator = SpiceEstimator(
     rnn_class=RNN_ContrDiff,
     spice_config=CONTRAST_CONFIG,
     n_participants=n_participants,
-    epochs=512,  # Quick test
+    epochs=8192,  # Quick test
     bagging=True,
     learning_rate=1e-2,  # Standard learning rate
     scheduler=True,
     train_test_ratio=test_blocks,
     spice_library_polynomial_degree=2,
-    use_optuna=False,  # Disable for faster testing
+    use_optuna=True,  # Disable for faster testing
     save_path_rnn=path_model,
     save_path_spice=path_model.replace('rnn', 'spice'),
     n_sessions_off_policy=1,
+    device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 )
 
-# estimator.fit(dataset.xs, dataset.ys)
-estimator.load_rnn_model(path_model)
-estimator.load_spice_model(path_model.replace('rnn', 'spice'))
+estimator.fit(dataset.xs, dataset.ys)
+# estimator.load_rnn_model(path_model)
+# estimator.load_spice_model(path_model.replace('rnn', 'spice'))
 
-estimator.print_spice_model()
+# estimator.print_spice_model()
 
-fig, axs = plot_session(
-    agents={
-        'rnn': estimator.rnn_agent,
-        'spice': estimator.spice_agent,
-        },
-    experiment=dataset.xs[16],
-    signals_to_plot=['x_value_reward', 'x_value_choice'],
-    display_choice=1,
-    )
-plt.show()
+# fig, axs = plot_session(
+#     agents={
+#         'rnn': estimator.rnn_agent,
+#         'spice': estimator.spice_agent,
+#         },
+#     experiment=dataset.xs[16],
+#     signals_to_plot=['x_value_reward', 'x_value_choice'],
+#     display_choice=1,
+#     )
+# plt.show()
