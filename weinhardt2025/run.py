0c1d3417f360941ea5844342665c7aa3abcc7b24
whyhardt <daniel.weinhardt@uni-osnabrueck.de>
2026-02-03 08:02:48 +0100

    commented out predefined data/model

--- weinhardt2025/run.py
+++ weinhardt2025/run.py
@@ -50,11 +50,11 @@ if __name__=='__main__':
     
     # args.sindy_weight = 0
     
-    args.eopchs = 10
-    args.model = "weinhardt2025/params/eckstein2022/spice_eckstein2022.pkl"
-    args.data = "weinhardt2025/data/eckstein2022/eckstein2022.csv"
-    args.train_ratio_time = 0.8
-    include_validation = False
+    # args.eopchs = 10
+    # args.model = "weinhardt2025/params/eckstein2022/spice_eckstein2022.pkl"
+    # args.data = "weinhardt2025/data/eckstein2022/eckstein2022.csv"
+    # args.train_ratio_time = 0.8
+    # include_validation = False
     
     # args.model = "weinhardt2025/params/eckstein2024/spice_eckstein2024.pkl"
     # args.data = "weinhardt2025/data/eckstein2024/eckstein2024.csv"
